c3b72831cc7ae44cc7dd676f4b0c5543ac6dfc60
kk7629340-cloud <kk7629340@gmail.com>
2026-02-26 05:15:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import asyncio
-import random
-from highrise import BaseBot, User, Position, run
+from highrise import BaseBot, User, Position
 from highrise.models import SessionMetadata
+from highrise.__main__ import main
 
 class MyBot(BaseBot):
     def __init__(self):
         super().__init__()
+        # المعلومات تاعك راهي هنا
         self.room_id = "68bb81b6b3ad4d2c8d06e70d"
         self.api_token = "a7bc6b92e7aab123e5a044d7ce4d252671ae12cf7554a58fe4c1012a882d8d4f"
 
@@ -13,6 +14,8 @@ class MyBot(BaseBot):
         await self.highrise.chat(f"مرحبا بيك يا {user.username} نورت الروم! ✨")
 
 if __name__ == "__main__":
-    run(MyBot(), "68bb81b6b3ad4d2c8d06e70d", "a7bc6b92e7aab123e5a044d7ce4d252671ae12cf7554a58fe4c1012a882d8d4f")
+    # استعملنا دالة main الصحيحة هنا
+    main(MyBot(), "68bb81b6b3ad4d2c8d06e70d", "a7bc6b92e7aab123e5a044d7ce4d252671ae12cf7554a58fe4c1012a882d8d4f")
+
 
     
